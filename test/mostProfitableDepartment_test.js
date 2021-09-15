ad96afa9952cc5ccf1ee1deba63f27c85fc7b6e5
Anele Silimela <silimela.anelle@gmail.com>
2021-09-15 19:50:10 +0200

    testing functions

--- test/mostProfitableDepartment_test.js
+++ test/mostProfitableDepartment_test.js
@@ -1,15 +1,27 @@
 
 let assert = require("assert");
-const mostProfitableDepartment = require('../mostProfitableDepartment');
+let findItemsOver = require("../findItemsOver");
 
-
-
-describe('the mostProfitableDepartment function ' , function(){
-    it('the functiion should identify the departments that are the most profitabl for both datasets 1 and 2' , function(){
-        assert.equal('', mostProfitableDepartment('salesData1'));
-        assert.equal('', mostProfitableDepartment('salesData2'))
-        
-    });
+describe('findItemsOver',function(){
+    it('should return all the products that have a quantity higher than 20',function(){
+        var salesData = [
+            {department : 'hardware', sales : 4500, day : 'Monday'},
+            {department : 'outdoor', sales : 1500, day : 'Monday'},
+            {department : 'carpentry', sales : 5500, day : 'Monday'},
+            {department : 'hardware', sales : 7500, day : 'Tuesday'},
+            {department : 'outdoor', sales : 2505, day : 'Tuesday'},
+            {department : 'carpentry', sales : 1540, day : 'Tuesday'},
+            {department : 'hardware', sales : 1500, day : 'Wednesday'},
+            {department : 'outdoor', sales : 8507, day : 'Wednesday'},
+            {department : 'carpentry', sales : 8009, day : 'Wednesday'},
+            {department : 'hardware', sales : 12000, day : 'Thursday'},
+            {department : 'outdoor', sales : 18007, day : 'Thursday'},
+            {department : 'carpentry', sales : 6109, day : 'Thursday'},
+            {department : 'hardware', sales : 7005, day : 'Friday'},
+            {department : 'outdoor', sales : 12006, day : 'Friday'},
+            {department : 'carpentry', sales : 16109, day : 'Friday'},
+        ];
+       
+    })
    
-
-});
+    });
